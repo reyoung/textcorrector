d9d098725a8623a28227c70af58309585a66e9f8
josephyu <josephyu@tencent.com>
2021-11-11 16:06:41 +0800

    Refine edit distance

--- tc/data/edit_distance.h
+++ tc/data/edit_distance.h
@@ -10,19 +10,21 @@ inline DistType EditDistance(const Container1 &a, const Container2 &b) {
   size_t min_size = a.size();
   size_t max_size = b.size();
   std::vector<DistType> lev_dist(min_size + 1);
-  for (size_t i = 0; i <= min_size; ++i) {
-    lev_dist[i] = i;
+  lev_dist[0] = 0;
+  for (size_t i = 1; i <= min_size; ++i) {
+    lev_dist[i] = lev_dist[i - 1] + 1;
   }
-  for (size_t j = 1; j < max_size; ++j) {
+  for (size_t j = 1; j <= max_size; ++j) {
     DistType previous_diagonal = lev_dist[0];
     DistType previous_diagonal_save;
-    ++lev_dist[0];
+    lev_dist[0] += 1;
+
     for (size_t i = 1; i <= min_size; ++i) {
       previous_diagonal_save = lev_dist[i];
       if (a[i - 1] == b[j - 1]) {
         lev_dist[i] = previous_diagonal;
       } else {
-        lev_dist[i] = std::min(std::min(lev_dist[i - 1], lev_dist[i]), previous_diagonal) + 1;
+        lev_dist[i] = std::min(std::min(lev_dist[i - 1] + 1, lev_dist[i] + 1), previous_diagonal + 1);
       }
       previous_diagonal = previous_diagonal_save;
     }
